c454217f07e5222f317b996743e8c36254aa670f
Максим <166473866+Macks3891@users.noreply.github.com>
2024-10-20 11:33:39 +0500

    Redoing20 Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        // Вызов метода square
+        int result1 = Methods.square(5);
+        System.out.println("Square of 5: " + result1);
+
+        int result2 = Methods.square(10);
+        System.out.println("Square of 10: " + result2);
+
+        // Вызов метода printFullName
+        System.out.println("Makcim" + "Zhirov");
+        System.out.println();
+
+        // Вызов метода cornerValuesArray для строк
+        String[] words = new String[]{"Java", "World java", "Hello Java World"};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(words)));
+
+
+        // Вызов метода cornerValuesArray для целых чисел
+        int[] intCornerValues = new int[]{3, 7, 1, 9, 4, 6};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(intCornerValues)));
+
+
+        // Вызов метода cornerValuesArray для символов
+        char[] charCornerValues = new char[]{'a', 'z', 'b', 'y', 'c'};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(charCornerValues)));
+
+        // Вызов метода factorial
+        int factorialResult = Methods.factorial(5);
+        System.out.println("Factorial of 5: " + factorialResult);
+
+        factorialResult = Methods.factorial(10); // Исправлено: добавлен класс Methods
+        System.out.println("Factorial of 10: " + factorialResult);
+
+        // Проверка на переполнение стека (базовый случай не определен)
+        Methods.factorial(-1); // Исправлено: добавлен класс Methods
     }
 }
+
+
+
